fd38c92cd7d8855415796c3dc991990f43ed0090
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-13 23:25:29 -0500

    Cambios Main
    se arreglaron datos y se creo los contadores para el momento de desencolar y saber cuantos fueron atendidos de la edad normal y de la tercera edad

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -7,14 +7,16 @@ import tadCola.TadCola;
 
 public class Principal {
 	public static void main(String[] args) {
+		Long start = System.nanoTime();
 		Persona[] personas; 
 		TadPila<Persona> pilaAtendidos = new TadPila<>("atendidos");
 		TadCola<Persona> colaNormal = new TadCola<>("normal");
 		TadCola<Persona> colaTerceraEdad = new TadCola<>("tercera edad");
-		
+		int countTerceraEdad = 0;
+		int countNormal = 0;
 		int n = 100;
 		personas = generarPersonas(n);
-		System.out.println("Se Ingresaron 100 Personas Al Centro de Ayuda");
+		
 		// las 10 primeras personas que entran al centro
 		for(int i = 0; i < 10; i++) {
 			if(personas[i].getEdad() >= 65) {
@@ -28,7 +30,7 @@ public class Principal {
 			Persona atendido;
 			if(personas[i].getEdad() >= 65) {
 				colaTerceraEdad.encolar(personas[i]);
-			}else if(personas[i].getEdad() < 65) {
+			}else{
 				colaNormal.encolar(personas[i]);
 				
 			}
@@ -36,7 +38,7 @@ public class Principal {
 			if(personas[i+1].getEdad() >= 65) {
 				colaTerceraEdad.encolar(personas[i+1]);
 			}
-			else if(personas[i+1].getEdad() < 65) {
+			else{
 				colaNormal.encolar(personas[i+1]);
 			}
 			
@@ -44,11 +46,24 @@ public class Principal {
 			try { // hay que incluir el try catch para capturar excepcion de desencolar
 			if(!colaTerceraEdad.colaVacia()) { // si no esta vacia la cola de tercera edad se atiende primordialmente a la tercera edad
 				atendido = colaTerceraEdad.desencolar();
+				countTerceraEdad++;
 				pilaAtendidos.apilar(atendido);
 			}else { // si esta vacia la cola de tercera edad se atiende a la cola normal
 				if(!colaNormal.colaVacia()) { // si la cola normal no esta vacia
 					atendido = colaNormal.desencolar(); // se atiende a la persona de la cola normal
+					countNormal++;
 					pilaAtendidos.apilar(atendido); // se agrega a la pila de atendidos
+				}
+			}
+			
+			if(i == 99) {
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println(" Se Ingresaron " + n + " Personas al centro de ayuda ");
+				System.out.println(" Se Atendieron a " + pilaAtendidos.numElemPila() + " Personas en  el centro de ayuda");
+				if(countTerceraEdad > countNormal) {
+					System.out.println("Se atendieron a " + countTerceraEdad + " personas de la tercera Edad " + " y " + countNormal + " personas de la edad normal");
+				}else {
+					System.out.println("Se atendieron a " + countNormal + " personas de la edad normal " + " y " + countTerceraEdad + " personas de la tercera edad");
 				}
 			}
 			}catch(ColaVacia e) {
@@ -64,13 +79,13 @@ public class Principal {
 			System.out.println("Se atendieron a " + colaNormal.numElemCola() + " personas de edad normal y " + colaTerceraEdad.numElemCola() + " personas de la tercera edad");
 		}
 		
-		
+		Long end = System.nanoTime();
 	}
 	
 	public static Persona[] generarPersonas(int n) {
 		Persona[] personas = new Persona[n];
 		for(int i = 0; i < n; i++) {
-			personas[i] = new Persona(Algoritmos.cedulaAleatorio("El Oro"), Algoritmos.aleatorio(18, 90));
+			personas[i] = new Persona(Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(18, 90));
 		}
 		
 		return personas;
